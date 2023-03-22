5099ad359ff9107a43f414cad94570b4a239d310
kroberts <cppwriter@yahoo.com>
2023-03-22 11:34:43 -0700

    some cleanup and an init delay

--- VirtualDesktopGridSwitcher/Program.cs
+++ VirtualDesktopGridSwitcher/Program.cs
@@ -1,24 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Windows.Forms;
 using VirtualDesktopGridSwitcher.Settings;
 
-namespace VirtualDesktopGridSwitcher {
-    static class Program {
+namespace VirtualDesktopGridSwitcher 
+{
+    static class Program
+    {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main() {
+        static void Main() 
+        {
+            // sometimes during startup it fails to init properly, so adding a slight delay
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
             var settings = SettingValues.Load();
-            using (var sysTrayProcess = new SysTrayProcess(settings)) {
-
+            using (var sysTrayProcess = new SysTrayProcess(settings)) 
+            {
                 using (var gridManager =
-                            new VirtualDesktopGridManager(sysTrayProcess, settings)) {
+                            new VirtualDesktopGridManager(sysTrayProcess, settings)) 
+                {
 
                     sysTrayProcess.SetClickSwitchHandler(gridManager.Switch);
 
